4c750207e4fee719c40b3285df336e216a580e67
ahmed-Abdelhameed <medoabdelhameed3@gmail.com>
2024-05-18 13:28:01 +0300

    hello

--- src/app/Modules/services/phone-countries-api.service.ts
+++ src/app/Modules/services/phone-countries-api.service.ts
@@ -10,9 +10,23 @@ export class PhoneCountriesAPIService {
   
   private apiUrl = environment.countriesCodes;
 
+  countries: any[] = [];
+
   constructor(private http: HttpClient) { }
 
   getCountryData(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
+  }
+
+  getCountriesArray(){
+    this.getCountryData().subscribe(data => {
+      for (let i = 0; i < data.length; i++) {
+        if (i == 44 || i == 63)
+          continue
+        this.countries.push(data[i].idd.root + data[i].idd.suffixes[0])
+      }
+      this.countries.sort().reverse()
+    })
+    return this.countries
   }
 }
